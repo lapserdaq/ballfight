46bfe7c50da97250fc5d4f81a32640451bc67f2e
Zmorka <kopczyk.michal@gmail.com>
2015-01-04 16:03:56 +0100

    Scene selection

---
+++ Assets/Scripts/Character/CharacterSelect.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var selectedCharacter : int = 0;
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetMouseButtonUp (0)){
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var hit : RaycastHit;
+		
+		if(Physics.Raycast (ray, hit, 100)){
+			if(hit.collider.name == "Character1"){
+				Debug.Log("Character1 selected");
+				selectedCharacter = 1;
+				PlayerPrefs.SetInt("selectedCharacter", (selectedCharacter));
+			}
+			if(hit.collider.name == "Character2"){
+				Debug.Log("Character2 selected");
+				selectedCharacter = 2;
+				PlayerPrefs.SetInt("selectedCharacter", (selectedCharacter));
+			}
+			if(hit.collider.name == "Character3"){
+				Debug.Log("Character3 selected");
+				selectedCharacter = 3;
+				PlayerPrefs.SetInt("selectedCharacter", (selectedCharacter));
+			}
+		}else{
+			return;
+		}
+	
+	}
+
+}
---
+++ Assets/Scripts/Character/CharacterSpawner.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+var character01Prefab : GameObject;
+var character02Prefab : GameObject;
+var character03Prefab : GameObject;
+
+var savedPlayer : int = 0;
+
+//runs before start
+function Awake(){
+
+	savedPlayer = PlayerPrefs.GetInt("selectedCharacter");
+	
+	character01Prefab = GameObject.Find("Character1");
+	character02Prefab = GameObject.Find("Character2");
+	character03Prefab = GameObject.Find("Character3");
+	
+	switch(savedPlayer){
+		case 0:
+			character01Prefab.SetActiveRecursively(true);
+			character02Prefab.SetActiveRecursively(false);
+			character03Prefab.SetActiveRecursively(false);
+			break;
+		case 1:
+			character01Prefab.SetActiveRecursively(true);
+			character02Prefab.SetActiveRecursively(false);
+			character03Prefab.SetActiveRecursively(false);
+			break;
+		case 2:
+			character01Prefab.SetActiveRecursively(false);
+			character02Prefab.SetActiveRecursively(true);
+			character03Prefab.SetActiveRecursively(false);
+			break;
+		case 3:
+			character01Prefab.SetActiveRecursively(false);
+			character02Prefab.SetActiveRecursively(false);
+			character03Prefab.SetActiveRecursively(true);
+			break;			
+	}
+
+}
